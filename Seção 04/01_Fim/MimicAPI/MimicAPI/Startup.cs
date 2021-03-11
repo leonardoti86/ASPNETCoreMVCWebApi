c427cdafcc89c5e59dfe7799e6d2127209ec9da5
leonardoti86 <leonardoti86@gmail.com>
2021-03-11 00:58:09 -0300

    criação API inicial

--- Seção 04/01_Fim/MimicAPI/MimicAPI/Startup.cs
+++ Seção 04/01_Fim/MimicAPI/MimicAPI/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using MimicAPI.Database;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,11 @@ namespace MimicAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddDbContext<MimicContext>(opt =>
+            {
+                opt.UseSqlite("Data source=Database\\Mimic.db"); 
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +32,7 @@ namespace MimicAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
         }
     }
 }
