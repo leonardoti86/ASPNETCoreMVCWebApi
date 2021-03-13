3dd9e7e476c210b8293c38258f38cff3494c93dc
leonardoti86 <leonardoti86@gmail.com>
2021-03-13 01:20:08 -0300

    database Sqlite, api Palavra,  paginação

--- Seção 04/01_Fim/MimicAPI/MimicAPI/Controllers/PalavrasController.cs
+++ Seção 04/01_Fim/MimicAPI/MimicAPI/Controllers/PalavrasController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MimicAPI.Database;
+using MimicAPI.Helpers;
 using MimicAPI.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,10 +24,40 @@ namespace MimicAPI.Controllers
         //app
         [Route("")]
         [HttpGet]
-        public ActionResult ObterTodas()
+        public ActionResult ObterTodas([FromQuery] PalavraUrlQuery query)
         {
+            var item = _banco.Palavras.AsQueryable();
+            if (query.Data.HasValue)
+            {
+                item = item.Where(x => x.Criado > query.Data.Value || x.Atualizado > query.Data.Value);
+            }
+
+            if (query.PagNumero.HasValue)
+            {
+                var quantidadeTotalRegistros = item.Count();
+
+                // usar os metodo Skip (pular) e Take (pegar)
+                // usar o .value pq os metodos de cima não aceitam nulos
+                item = item.Skip((query.PagNumero.Value - 1) * query.PagRegistro.Value).Take(query.PagRegistro.Value);
+
+                Paginacao paginacao = new Paginacao()
+                {
+                    NumeroPagina = query.PagNumero.Value,
+                    RegistroPorPagina = query.PagRegistro.Value,
+                    TotalRegistros = quantidadeTotalRegistros,
+                    TotalPaginas = (int)Math.Ceiling((double)quantidadeTotalRegistros / query.PagRegistro.Value) /*usa o Ceiling para arredondar para cima o total de paginas*/
+                };
+
+                Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(paginacao));
+
+                if (query.PagNumero > paginacao.TotalPaginas)
+                {
+                    return NotFound();
+                }
+            }
+
             //return new JsonResult(_banco.Palavras);
-            return Ok(_banco.Palavras);
+            return Ok(item);
         }
 
         //web
@@ -32,24 +65,37 @@ namespace MimicAPI.Controllers
         [HttpGet]
         public ActionResult Obter(int id)
         {
-            return Ok(_banco.Palavras.Find(id));
+            var obj = _banco.Palavras.Find(id);
+
+            if (obj == null)
+                return NotFound();
+
+            return Ok(obj);
         }
 
         [Route("")]
         [HttpPost]
-        public ActionResult Cadastrar(Palavra palavra)
+        public ActionResult Cadastrar([FromBody] Palavra palavra)
         {
             _banco.Palavras.Add(palavra);
+            _banco.SaveChanges();
 
-            return Ok();
+            return Created($"/api/palavras/{palavra.Id}", palavra);
         }
 
         [Route("{id}")]
         [HttpPut]
-        public ActionResult Atualizar(int id, Palavra palavra)
+        public ActionResult Atualizar(int id, [FromBody] Palavra palavra)
         {
+            //var obj = _banco.Palavras.Find(id);
+            var obj = _banco.Palavras.AsNoTracking().FirstOrDefault(a => a.Id.Equals(id)); //usa-se o AsNoTracking para evitar exception de objeto nulo
+
+            if (obj == null)
+                return NotFound();
+
             palavra.Id = id;
             _banco.Palavras.Update(palavra);
+            _banco.SaveChanges();
 
             return Ok();
         }
@@ -58,9 +104,18 @@ namespace MimicAPI.Controllers
         [HttpDelete]
         public ActionResult Deletar(int id)
         {
-            _banco.Palavras.Remove(_banco.Palavras.Find(id));
+            var palavra = _banco.Palavras.Find(id);
 
-            return Ok();
+            if (palavra == null)
+                return NotFound();
+
+            palavra.Ativo = false;
+            _banco.Palavras.Update(palavra);
+            _banco.SaveChanges();
+
+            //_banco.Palavras.Remove(_banco.Palavras.Find(id));
+
+            return NoContent();
         }
     }
 }
