ff981eabb44f4d13fed50ffbbeece5f8e92989fb
leonardoti86 <leonardoti86@gmail.com>
2021-03-14 00:58:56 -0300

    PalavraRepository e AutoMapper

--- Seção 04/01_Fim/MimicAPI/MimicAPI/Controllers/PalavrasController.cs
+++ Seção 04/01_Fim/MimicAPI/MimicAPI/Controllers/PalavrasController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using MimicAPI.Database;
 using MimicAPI.Helpers;
 using MimicAPI.Models;
+using MimicAPI.Models.DTO;
+using MimicAPI.Repositories.Contracts;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -14,105 +16,87 @@ namespace MimicAPI.Controllers
     [Route("api/palavras")]
     public class PalavrasController : ControllerBase
     {
-        private readonly MimicContext _banco;
+        private readonly IPalavraRepository _repository;
+        private readonly IMapper _mapper;
 
-        public PalavrasController(MimicContext banco)
+
+        public PalavrasController(IPalavraRepository repository, IMapper mapper)
         {
-            _banco = banco;
+            _repository = repository;
+            _mapper = mapper;
         }
 
-        //app
         [Route("")]
         [HttpGet]
         public ActionResult ObterTodas([FromQuery] PalavraUrlQuery query)
         {
-            var item = _banco.Palavras.AsQueryable();
-            if (query.Data.HasValue)
+            var item = _repository.ObterPalavras(query);
+
+            if (query.PagNumero != null)
             {
-                item = item.Where(x => x.Criado > query.Data.Value || x.Atualizado > query.Data.Value);
-            }
-
-            if (query.PagNumero.HasValue)
-            {
-                var quantidadeTotalRegistros = item.Count();
-
-                // usar os metodo Skip (pular) e Take (pegar)
-                // usar o .value pq os metodos de cima não aceitam nulos
-                item = item.Skip((query.PagNumero.Value - 1) * query.PagRegistro.Value).Take(query.PagRegistro.Value);
-
-                Paginacao paginacao = new Paginacao()
-                {
-                    NumeroPagina = query.PagNumero.Value,
-                    RegistroPorPagina = query.PagRegistro.Value,
-                    TotalRegistros = quantidadeTotalRegistros,
-                    TotalPaginas = (int)Math.Ceiling((double)quantidadeTotalRegistros / query.PagRegistro.Value) /*usa o Ceiling para arredondar para cima o total de paginas*/
-                };
-
-                Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(paginacao));
-
-                if (query.PagNumero > paginacao.TotalPaginas)
-                {
+                if (query.PagNumero > item.Paginacao.TotalPaginas)
                     return NotFound();
-                }
             }
 
-            //return new JsonResult(_banco.Palavras);
-            return Ok(item);
+            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(item.Paginacao));
+
+            return Ok(item.ToList());
         }
 
-        //web
-        [Route("{id}")]
-        [HttpGet]
+        //[Route("{id}")] //usado no parametro do HttpGet em baixo. isso corrige o problema de enviar via queryString o id.
+        [HttpGet("{id}", Name = "ObterPalavra")]
         public ActionResult Obter(int id)
         {
-            var obj = _banco.Palavras.Find(id);
+            var obj = _repository.Obter(id);
 
             if (obj == null)
                 return NotFound();
 
-            return Ok(obj);
+            PalavraDTO palavraDTO = _mapper.Map<Palavra, PalavraDTO>(obj);
+            palavraDTO.Links = new List<LinkDTO>();
+            palavraDTO.Links.Add(new LinkDTO("self", Url.Link("ObterPalavra", new { id = palavraDTO.Id }),"GET")); //Url.Link() é bom pra efeito de implantação. deixa flexível o link
+            palavraDTO.Links.Add(new LinkDTO("update", Url.Link("AtualizarPalavra", new { id = palavraDTO.Id }), "PUT"));
+            palavraDTO.Links.Add(new LinkDTO("delete", Url.Link("ExcluirPalavra", new { id = palavraDTO.Id }), "DELETE"));
+
+            //palavraDTO.Links
+
+            return Ok(palavraDTO);
         }
 
         [Route("")]
         [HttpPost]
         public ActionResult Cadastrar([FromBody] Palavra palavra)
         {
-            _banco.Palavras.Add(palavra);
-            _banco.SaveChanges();
+            _repository.Cadastrar(palavra);
 
             return Created($"/api/palavras/{palavra.Id}", palavra);
         }
 
-        [Route("{id}")]
-        [HttpPut]
+        //[Route("{id}")]
+        [HttpPut("{id}",Name = "AtualizarPalavra")]
         public ActionResult Atualizar(int id, [FromBody] Palavra palavra)
         {
-            //var obj = _banco.Palavras.Find(id);
-            var obj = _banco.Palavras.AsNoTracking().FirstOrDefault(a => a.Id.Equals(id)); //usa-se o AsNoTracking para evitar exception de objeto nulo
+            var obj = _repository.Obter(id);
 
             if (obj == null)
                 return NotFound();
 
             palavra.Id = id;
-            _banco.Palavras.Update(palavra);
-            _banco.SaveChanges();
+            _repository.Atualizar(palavra);
 
             return Ok();
         }
 
         [Route("{id}")]
-        [HttpDelete]
+        [HttpDelete("{id}", Name = "ExcluirPalavra")]
         public ActionResult Deletar(int id)
         {
-            var palavra = _banco.Palavras.Find(id);
+            var palavra = _repository.Obter(id);
 
             if (palavra == null)
                 return NotFound();
 
-            palavra.Ativo = false;
-            _banco.Palavras.Update(palavra);
-            _banco.SaveChanges();
-
+            _repository.Deletar(id);
             //_banco.Palavras.Remove(_banco.Palavras.Find(id));
 
             return NoContent();
--- Seção 04/01_Fim/MimicAPI/MimicAPI/Startup.cs
+++ Seção 04/01_Fim/MimicAPI/MimicAPI/Startup.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MimicAPI.Database;
+using MimicAPI.Helpers;
+using MimicAPI.Repositories;
+using MimicAPI.Repositories.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,11 +21,26 @@ namespace MimicAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //injeta MVC
             services.AddMvc();
+
+            //injeta EF usando o Sqlite
             services.AddDbContext<MimicContext>(opt =>
             {
                 opt.UseSqlite("Data source=Database\\Mimic.db"); 
             });
+
+            //injeta no escopo a relação dessa interface com classe
+            services.AddScoped<IPalavraRepository,PalavraRepository>();
+
+            //Configura AutoMapper
+            var config = new MapperConfiguration(cfg => {
+                cfg.AddProfile(new DTOMapperProfile());
+            });
+            IMapper mapper = config.CreateMapper();
+            services.AddSingleton(mapper); // uma única instancia na aplicação utilizando esse "mapeador"
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
