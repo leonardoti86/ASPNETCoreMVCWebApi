198f41bbaef3b874dd47c67fa396179068d24582
leonardoti86 <leonardoti86@gmail.com>
2021-03-19 00:57:43 -0300

    inicio MinhasTarefasAPI

---
+++ Seção 05/01_Fim/MinhasTarefasAPI/MinhasTarefasAPI/Repositories/TarefaRepository.cs
@@ -0,0 +1,43 @@
+﻿using MinhasTarefasAPI.Database;
+using MinhasTarefasAPI.Models;
+using MinhasTarefasAPI.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhasTarefasAPI.Repositories
+{
+    public class TarefaRepository : ITarefaRepository
+    {
+        private readonly MinhasTarefasContext _banco;
+
+        public TarefaRepository(MinhasTarefasContext banco)
+        {
+            _banco = banco;
+        }
+
+        public List<Tarefa> Restauracao(ApplicationUser usuario, DateTime dataUltimaRestauracao)
+        {
+            var query = _banco.Tarefas.Where(a => a.UsuarioId == usuario.Id).AsQueryable();
+
+            if (dataUltimaRestauracao == null)
+            {
+                query.Where(a => a.Criado >= dataUltimaRestauracao || a.Atualizado >= dataUltimaRestauracao);
+            }
+
+            return query.ToList<Tarefa>();
+        }
+
+        public void Sincronizacao(List<Tarefa> tarefas)
+        {
+            //cadastrar novos registros
+
+            //atualização de registro (excluido)
+
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
