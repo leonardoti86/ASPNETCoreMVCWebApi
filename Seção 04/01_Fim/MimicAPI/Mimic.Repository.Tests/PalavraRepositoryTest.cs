c526e8a5872120e7ee4ca4d61d73b8bcb17a71f1
leonardoti86 <leonardoti86@gmail.com>
2021-04-29 22:40:57 -0300

    projeto de teste Repository

---
+++ Seção 04/01_Fim/MimicAPI/Mimic.Repository.Tests/PalavraRepositoryTest.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MimicAPI.v1.Repositories;
+using MimicAPI.v1.Repositories.Contracts;
+
+namespace Mimic.Repository.Tests
+{
+    [TestClass]
+    public class PalavraRepositoryTest
+    {
+        public void DeveAdicionarUmaPalavra()
+        {
+            Assert.AreEqual("teste", "teste");
+        }
+    }
+}
